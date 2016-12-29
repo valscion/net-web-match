946b59ffc439727f5dc146f661d9273040c49051
Vesa Laakso <laakso.vesa@gmail.com>
2016-12-29 10:13:58 +0200

    Assume server lives in 1 port higher than the current served URL

--- source/client.js
+++ source/client.js
@@ -16,6 +16,10 @@ var Client = IgeClass.extend({
 
     self._addEditor();
 
+    var host = location.hostname;
+    var port = Number(location.port) + 1;
+    var serverUrl = 'http://' + host + ':' + port;
+
     // Wait for our textures to load before continuing
     ige.on('texturesLoaded', function () {
       // Create the HTML canvas
@@ -30,7 +34,7 @@ var Client = IgeClass.extend({
           // than before the scene etc are created... maybe you want
           // a splash screen or a menu first? Then connect after you've
           // got a username or something?
-          ige.network.start('http://' + location.hostname + ':2000', function () {
+          ige.network.start(serverUrl, function () {
             ige.network.debug(true);
             ige.network.define('playerEntity', self._onPlayerEntity); // Defined in ./gameClasses/ClientNetworkEvents.js
 
