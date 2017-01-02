207c2c296eacb267c1d44426a788056ba5728d6c
Vesa Laakso <laakso.vesa@gmail.com>
2017-01-02 22:05:49 +0200

    Add tweening to camera zoomer

--- source/gameClasses/CameraZoomer.js
+++ source/gameClasses/CameraZoomer.js
@@ -9,9 +9,17 @@ var CameraZoomer = IgeClass.extend({
   _onKeyDown: function (_event, keyCode) {
     if (keyCode === ige.input.key.z) {
       if (this._isZooming) {
-        ige.client.vp1.camera.scaleTo(1, 1, 0);
+        ige.client.vp1.camera._scale.tween()
+          .stopAll()
+          .properties({x: 1, y: 1})
+          .duration(500)
+          .start();
       } else {
-        ige.client.vp1.camera.scaleTo(0.25, 0.25, 0);
+        ige.client.vp1.camera._scale.tween()
+          .stopAll()
+          .properties({x: 0.25, y: 0.25})
+          .duration(500)
+          .start();
       }
 
       this._isZooming = !this._isZooming;
